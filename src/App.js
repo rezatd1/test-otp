177bd4e2da55331e4599b1036e397ce55e86e3f8
Reza Torkashvand <reza.torkashvand.ins@gmail.com>
2023-10-08 10:54:39 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({
+    countryCode: 98,
+    mobileWithOutCountryCode: '9390753192',
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch('http://172.17.17.101:8088/api/en/Account/SignUp/SendVerificationCodeForResetPassword', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        // Handle the response data
+        console.log(responseData);
+      })
+      .catch((error) => {
+        // Handle any error that occurred
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Country Code:
+        <input
+          type="number"
+          name="countryCode"
+          value={data.countryCode}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>
+        Mobile Number:
+        <input
+          type="text"
+          name="mobileWithOutCountryCode"
+          value={data.mobileWithOutCountryCode}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
 
 export default App;
